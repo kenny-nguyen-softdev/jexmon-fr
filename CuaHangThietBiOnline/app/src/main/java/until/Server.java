7324a69bc215bc35494002acfbf7ae343b6c8c2d
dung0mg5 <20110452@student.hcmute.edu.vn>
2023-05-18 22:51:46 +0700

    up

--- CuaHangThietBiOnline/app/src/main/java/until/Server.java
+++ CuaHangThietBiOnline/app/src/main/java/until/Server.java
@@ -7,7 +7,6 @@ public class Server {
     public static String Đuongandienthoai = "http://" + localhost + "/jexmon-bk/gettheosp.php?page=";
     public static String Duongdandonhang = "http://" + localhost + "/jexmon-bk/thongtinkhachhang.php";
     public static String Duongdanchitietdonhang = "http://" + localhost + "/jexmon-bk/chitietdonhang.php";
-
     public static String loginRequestUrl = String.format("http://%s//jexmon-bk/login.php", localhost);
     public static String regRequestUrl = String.format("http://%s//jexmon-bk/reg.php", localhost);
 
