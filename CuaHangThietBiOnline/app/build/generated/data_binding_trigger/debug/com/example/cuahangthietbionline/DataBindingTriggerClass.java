05283307f2bba35921cef6d54217cfcd52311b25
dung0mg5 <20110452@student.hcmute.edu.vn>
2023-05-19 09:29:37 +0700

    UP

--- CuaHangThietBiOnline/app/build/generated/data_binding_trigger/debug/com/example/cuahangthietbionline/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.cuahangthietbionline;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- app/src/main/java/until/Server.java
+++ app/src/main/java/until/Server.java
@@ -1,12 +1,13 @@
 package until;
 
 public class Server {
-    public static String localhost = "172.17.20.35";
+    public static String localhost = "207.148.76.121";
     public static String Đuonganloaisp = "http://" + localhost + "/jexmon-bk/getloaisp.php";
     public static String Đuongấnnphammoinhat = "http://" + localhost + "/jexmon-bk/getsanpham.php";
     public static String Đuongandienthoai = "http://" + localhost + "/jexmon-bk/gettheosp.php?page=";
     public static String Duongdandonhang = "http://" + localhost + "/jexmon-bk/thongtinkhachhang.php";
     public static String Duongdanchitietdonhang = "http://" + localhost + "/jexmon-bk/chitietdonhang.php";
+
     public static String loginRequestUrl = String.format("http://%s//jexmon-bk/login.php", localhost);
     public static String regRequestUrl = String.format("http://%s//jexmon-bk/reg.php", localhost);
 
